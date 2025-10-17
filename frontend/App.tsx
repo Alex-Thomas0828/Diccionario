209881298c1e579bc2d761ba597269b09d850f15
Alex.thomas0828 <Alex.thomas0828@gmail.com>
2025-10-17 11:57:01 -0500

    Screen navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
 import DictionaryScreen from './src/screens/DictionaryScreen';
 import WordEditorScreen from './src/screens/WordEditorScreen';
+import ContribuirScreen from './src/screens/ContribuirScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +26,11 @@ export default function App() {
           component={WordEditorScreen}
           options={{ title: 'Editar' }}
         />
+        <Stack.Screen 
+        name="Contribuir" 
+        component={ContribuirScreen}
+        options={{ title: 'Contribuir' }}
+  />
       </Stack.Navigator>
     </NavigationContainer>
   );
